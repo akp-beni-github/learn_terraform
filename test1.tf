a2d0020176ea8223e103f96b2243669dadd85f73
Ben <bengzio111@gmail.com>
2024-06-13 20:03:20 +0700

    create ec2 instance and connect to aws using aws-cli

--- test1.tf
+++
@@ -1,7 +0,0 @@
-#create txt file in local machine using terraform
-
-resource "null_resource" "file" { # null resource since it local machine not cloud
-  provisioner "local-exec" {
-    command = "echo 'Message: ${upper("hello world")}' > challenge1.txt "
-  }
-}
